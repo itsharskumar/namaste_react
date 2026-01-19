9c279e47e35ad635612dce91c94ba2ffc8150e3a
Harsh Kumar <harskumarop@gmail.com>
2026-01-20 01:05:05 +0530

    just checking

--- App.js
+++ App.js
@@ -1,7 +1,9 @@
 const parent=React.createElement(
     "div",{id:"parent"},
         React.createElement("div",{id:"child"},
-            React.createElement("h1",{},"I am a big guy")
+            [React.createElement("h1",{},"I am a big guy"),
+                React.createElement("h2",{},"I am his brother")
+            ]
         )
     
 );//this is a object not a html tag it is converted using redring convert it into html
