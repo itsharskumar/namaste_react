f2b6a12e5961b49caec9bff477fd9cba31e749ce
Harsh Kumar <harskumarop@gmail.com>
2026-01-19 23:34:40 +0530

    pushing app.js

---
+++ App.js
@@ -0,0 +1,9 @@
+const parent=React.createElement(
+    "div",{id:"parent"},
+        React.createElement("div",{id:"child"},
+            React.createElement("h1",{},"I am a big guy")
+        )
+    
+);//this is a object not a html tag it is converted using redring convert it into html
+const root=ReactDOM.createRoot(document.getElementById("root"));
+root.render(parent);
