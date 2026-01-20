e4366f603e5e9ece75dc7669199d6c3c4140299f
Harsh Kumar <harskumarop@gmail.com>
2026-01-20 18:28:13 +0530

    just checking

--- App.js
+++ App.js
@@ -1,11 +1,14 @@
+import React from "react"
+// import ReactDOM from "react-dom"
+import { createRoot } from "react-dom/client";
 const parent=React.createElement(
     "div",{id:"parent"},
         React.createElement("div",{id:"child"},
-            [React.createElement("h1",{},"I am a big guy"),
-                React.createElement("h2",{},"I am his brother")
+            [React.createElement("h1",{key:"h1"},"I am a big guy"),
+                React.createElement("h2",{key:"h2"},"I am his brother")
             ]
         )
     
 );//this is a object not a html tag it is converted using redring convert it into html
-const root=ReactDOM.createRoot(document.getElementById("root"));
+const root=createRoot(document.getElementById("root"));
 root.render(parent);
