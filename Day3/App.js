de611a45e293027fb89bdca1af5a050d614824f7
Harsh Kumar <harskumarop@gmail.com>
2026-01-21 19:08:21 +0530

    auto commit

--- Day3/App.js
+++ Day3/App.js
@@ -1,34 +1,63 @@
 import React from "react"
 import ReactDOM from "react-dom/client"
 
-//React Element
-// const heading=React.createElement("h1",{id:"heading"},"Namaste Harsh");
 
 
-//jsx (transpiled before it reaches the js) - PARCEL -Babel convert that react will understand
-// const jsxHeading=(
-// <h1 className="heading">Namaste Harsh</h1>
-// )//react element
 
-// React Component
-//class based component ->old
-// functional component ->new Way of writing code
+const Header=()=>{
+    return (
+        <div className="header">
+            <div className="logo-container">
+                <img className="logo" src="https://marketplace.canva.com/EAFaFUz4aKo/3/0/1600w/canva-yellow-abstract-cooking-fire-free-logo-tn1zF-_cG9c.jpg"/>
+            </div>
 
-const Title =()=>{
-    return <h2>Just checking inside the functional Component</h2>
+            <div className="nav-items">
+
+                <ul>
+                    <li>Home</li>
+                    <li>About</li>
+                    <li>ContactUs</li>
+                    <li>Cart</li>
+                </ul>
+            </div>
+        </div>
+    )
+}
+
+const ResturantCard =()=>{
+    return (
+        <div className="res-card">
+            <h3>Meghna Foods</h3>
+        </div>
+    )
+}
+
+const Body =()=>{
+    return (
+        <div className="body">
+
+            <div className="search">Search</div>
+            <div className="res-container">
+                <ResturantCard></ResturantCard>
+            </div>
+        </div>
+    )
 }
 
 
-const HeadingComponent=()=>(
-    <div id="container">
-        <Title></Title>
-       <h1 className="heading">Namaste Harsh</h1>
 
-    </div>
-)
+
+const AppLayout =()=>{
+    return (
+        <div className="app">
+            <Header></Header>
+            <Body></Body>
+        </div>
+    )
+}
 
 const root=ReactDOM.createRoot(document.getElementById("root"));
-root.render(<HeadingComponent></HeadingComponent>)
-// root.render(jsxHeading);//this is not a good way of doing it
+root.render(<AppLayout></AppLayout>)
+
 
 
