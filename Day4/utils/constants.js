c24a4577e7fc7ee62d9690403442acc119c50fad
Harsh Kumar <harskumarop@gmail.com>
2026-01-23 14:00:43 +0530

    make Day4 copy of previous learning new concept

---
+++ Day4/utils/constants.js
@@ -0,0 +1,5 @@
+export const CDN_URL =
+  "https://media-assets.swiggy.com/swiggy/image/upload/w_300,h_200,c_fill/";
+
+
+export const LOGO_URL="https://marketplace.canva.com/EAFaFUz4aKo/3/0/1600w/canva-yellow-abstract-cooking-fire-free-logo-tn1zF-_cG9c.jpg"
